a7ebef9dbebc8b773201239f5a64fce7a09a7411
adun <adnan2765rabbani@gmail.com>
2026-06-07 13:02:48 +0700

    fix(config): dynamically configure allowedDevOrigins with local IPs to allow dev HMR on mobile devices

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
+import os from "os";
+
+const getLocalIPs = (): string[] => {
+  const interfaces = os.networkInterfaces();
+  const ips: string[] = ["localhost", "127.0.0.1"];
+  
+  for (const name of Object.keys(interfaces)) {
+    const netInterface = interfaces[name];
+    if (!netInterface) continue;
+    
+    for (const iface of netInterface) {
+      if (!iface.internal && iface.family === "IPv4") {
+        ips.push(iface.address);
+      }
+    }
+  }
+  return ips;
+};
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: getLocalIPs(),
 };
 
 export default nextConfig;
